5a18193ba254d7c4c081b2e3e3e4fc9bb3afb2df
TienDungo <vtdung.dhkl17a1hn@sv.uneti.edu.vn>
2023-10-30 15:35:24 +0700

    Baitap

---
+++ #BÀI2.py
@@ -0,0 +1,12 @@
+#BÀI2
+x=10
+y=5
+a=x+y
+b=x-y 
+c=x*y
+d=x/y
+print('Tổng:',x,'+',y,'=',a)
+print('Hiệu:',x,"-",y,'=',b)
+print('Nhân:',x,'*',y,'=',c)
+print('Thương:',x,'/',y,'=',d)
+
---
+++ #Bài 4.py
@@ -0,0 +1,8 @@
+#Bài 4
+import math
+a=float(input("4"))
+b=float(input("5"))
+c=float(input("6"))
+p=(a+b+c)/2
+s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+print("diện tích tam giác là:",s)
